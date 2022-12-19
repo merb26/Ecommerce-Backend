c11fc49aa2cf20a7acd626a0d327f7ca6a72b4ef
Manuel Esteban (Merb) <manuele.ramirez.26@gmail.com>
2022-12-19 08:52:43 -0600

    Funcionando con otro framework

---
+++ Desafíos/Reformar para usar otro framework/sails-backend/api/models/Products.js
@@ -0,0 +1,35 @@
+/**
+ * Products.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  attributes: {
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    name: { type: 'string', required: true },
+    description: { type: 'string' },
+    code: { type: 'string', required: true },
+    urlPicture: { type: 'string', required: true },
+    price: { type: 'string', required: true },
+    stock: { type: 'string', required: true },
+  },
+};
+
+// {
+//   "name": "samsung",
+//   "description": "celular",
+//   "code": "123",
+//   "urlPicture": "ruta de foto",
+//   "price": "5000",
+//   "stock": "16"
+// }
